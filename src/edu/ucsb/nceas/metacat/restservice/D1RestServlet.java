49868d9dd561638f9f6cf42b1954bccd2cdc21b4
Thomas <thomas@Thomass-MacBook-Pro.local>
2026-04-08 09:32:59 -0700

    Add charset to rest service

--- src/edu/ucsb/nceas/metacat/restservice/D1RestServlet.java
+++ src/edu/ucsb/nceas/metacat/restservice/D1RestServlet.java
@@ -126,7 +126,7 @@ public class D1RestServlet extends HttpServlet {
             if (!isMetacatConfigured) {
                 ServiceFailure error = new ServiceFailure("0000", "Metacat has not been configured"
                            + ". Please go to https://your-host/metacat/admin to configure it");
-                setResponseContentType("text/xml");
+                response.setContentType("text/xml; charset=UTF-8");
                 response.setStatus(error.getCode());
                 try (OutputStream out = response.getOutputStream()) {
                     IOUtils.write(error.serialize(BaseException.FMT_XML), out, "UTF-8");
